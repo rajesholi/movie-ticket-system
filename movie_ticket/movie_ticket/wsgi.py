2d2dfb90f044c8bfa0afa25a33a0502a08fa89c4
Rajesh Chhetri <rajeshthedeveloper2004@gmail.com>
2026-05-21 16:05:23 +0545

    fix

--- movie_ticket/movie_ticket/wsgi.py
+++ movie_ticket/movie_ticket/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'movie_ticket.settings')
 
+# Django's default variable
 application = get_wsgi_application()
+
+# Add this line explicitly so Vercel can find the entry point
+app = application
